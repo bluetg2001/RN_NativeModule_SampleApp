0d79aa4376920505cddee1dbc0b6aaba5ce44539
gyun <45479579+bluetg2001@users.noreply.github.com>
2023-02-14 19:41:35 +0900

    플랫폼별 컴포넌트 분리

---
+++ containers/IosContainer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+  NativeModules,
+  Button,
+  Text,
+  Platform,
+} from 'react-native';
+
+import {Header} from 'react-native/Libraries/NewAppScreen';
+
+function IosContainer() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Header />
+        <View>
+          <Text style={{textAlign: 'center'}}>hihi</Text>
+          <Button title="Call In Native Code" />
+          <Button title="Call In React Native Code" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default IosContainer;
